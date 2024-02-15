1573d09e75b0e80bf2aa5529407e95e720d8c8cc
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2024-02-14 17:44:48 -0800

    artist model to handle track storage

---
+++ models/artist.js
@@ -0,0 +1,55 @@
+const db = require('../db');
+const { NotFoundError } = require('../expressError');
+
+class Artist {
+  /**
+   * fetch all artists
+   */
+  static async getAll() {
+    const query = `
+      SELECT id, name, popularity
+      FROM artists
+      ORDER BY popularity DESC`;
+    const result = await db.query(query);
+    return result.rows;
+  }
+
+  /**
+   * fetch an artists tracks
+   */
+  static async getTracks(artistId) {
+    const query = `
+      SELECT id, track_spotify_id, artist_id, date_added
+      FROM tracks
+      WHERE artist_id = $1
+      ORDER BY date_added DESC
+      LIMIT 10`;
+    const result = await db.query(query, [artistId]);
+    return result.rows;
+  }
+
+  /**
+   * delete all tracks for an artist
+   */
+  static async deleteTracks(artistId) {
+    const query = `
+      DELETE FROM tracks
+      WHERE artist_id = $1`;
+    const result = await db.query(query, [artistId]);
+    return result.rows;
+  }
+
+  /**
+   * add a track for an artist
+   */
+  static async addTrack(artistId, trackSpotifyId) {
+    const query = `
+      INSERT INTO tracks (artist_id, track_spotify_id)
+      VALUES ($1, $2)
+      RETURNING id, artist_id, track_spotify_id, date_added`;
+    const result = await db.query(query, [artistId, trackSpotifyId]);
+    return result.rows[0];
+  }
+
+}
+module.exports = Artist;
