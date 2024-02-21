b029991ff268d5ea7614b5ff67cb9707c0e9faee
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2024-02-20 16:26:53 -0800

    test because I changed remote git

--- db.js
+++ db.js
@@ -4,7 +4,7 @@
 
 const { Client } = require("pg");
 require('dotenv').config();
-
+//testing
 // Create a new client instance without specifying connection details.
 // The pg library will automatically use environment variables
 // such as PGUSER, PGPASSWORD, PGHOST, PGPORT, and PGDATABASE.
