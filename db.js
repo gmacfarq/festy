0c129506e30c48ede4c65e174a9abdc45a2bad88
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2024-02-25 15:57:44 -0800

    deployment with aws rds and ec2

--- db.js
+++ db.js
@@ -4,12 +4,33 @@
 
 const { Client } = require("pg");
 require('dotenv').config();
-//testing
+const f2 = reuire('fs');
+
+const sslConfig = {
+  rejectUnauthorized: true, // This should be true to verify the server's certificate against the CA
+  ca: fs.readFileSync('./us-west-1-bundle.pem').toString(), // Adjust the path as necessary
+};
+
 // Create a new client instance without specifying connection details.
 // The pg library will automatically use environment variables
 // such as PGUSER, PGPASSWORD, PGHOST, PGPORT, and PGDATABASE.
-const db = new Client();
+const db = new Client(
+  {
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+    database: process.env.PGDATABASE,
+    ssl: sslConfig,
+  }
+);
 
-db.connect();
+db.connect((err) => {
+  if (err) {
+    console.error('Database connection failed: ' + err.stack);
+    return;
+  }
+  console.log('Connected to database.');
+});
 
 module.exports = db;
